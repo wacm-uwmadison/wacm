267853245d905b0fcdad10dae76bff67515d2fa1
zkung707224 <zkung@wisc.edu>
2024-11-22 15:17:36 -0600

    Update website

--- events/upcoming_events_list.js
+++ events/upcoming_events_list.js
@@ -13,7 +13,7 @@ const upcoming_events_list = [
     "date": { "month": "November", "day": "25", "time": "12:00 pm - 1:00 pm" },
     "title": "WACM BiWeekly Lunch with Google Engineer",
     "description": "Join us for our last biweekly lunch next week December 11th at 12-1pm in CS1240!!\nWe will be having a google engineer and UW-Madison alum as a guest speaker share her story and experience as a woman in tech. Come get some Ian's pizza and learn!",
-    "imgSrc": "images/biweekly112224.png",
+    "imgSrc": "images/Biweekly112224.png",
     "rsvpLink": "https://forms.gle/vSARB9pi9UwMATuT7",
     "location": "Computer Sciences 1240"
 },
