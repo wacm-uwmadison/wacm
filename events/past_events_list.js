3455c82ec5c4613165625ec0cc15f5ecdca2b8f4
zkung707224 <zkung@wisc.edu>
2024-11-25 17:17:30 -0600

    Update website

--- events/past_events_list.js
+++ events/past_events_list.js
@@ -10,6 +10,14 @@
 
 const past_events_list = [
   {
+    "date": { "month": "December", "day": "2", "time": "4:00 pm - 5:00 pm" },
+    "title": "Internship Prep Talk",
+    "description": "Join our President Kayley for the annual WACM Internship Preparation Talk, designed to help students navigate the internship application process with confidence. Kayley will share tips on crafting strong resumes, preparing applications, standing out in interviews, and exploring alternative ways to gain valuable experience if your internships aren’t secured. A mini resume review will also take place—upload your resume through the RSVP form for a chance to receive personalized feedback during the session!\nThe talk will conclude with a Q&A, giving attendees an opportunity to ask questions and receive personal advice. While this event is geared toward underclassmen and WACM members, all students are welcome to attend. Don’t miss this opportunity to gain invaluable insights and jumpstart your career in the computer science major!",
+    "imgSrc": "images/internshiptalks.png",
+    "rsvpLink": "https://forms.gle/h9MjaUG5h4ntvLoL8",
+    "location": "Computer Sciences 1240"
+  },
+  {
     "date": { "month": "November", "day": "13", "time": "1:00 pm - 2:00 pm CT" },
     "title": "Biweekly Lunch: Course Edition",
     "description": "Hello everyone! We hope you are all taking time to yourself today and taking care of your mental health. We are here for any support always. Here is our flyer for our next Biweekly Lunch next week Wednesday Nov. 13 1-2pm!! We hope to see everyone there as we'll be connecting with each other about classes and asking for course enrollment advice. + Free Lunch!!!",
