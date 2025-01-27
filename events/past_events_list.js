ab47ba821387bf262ebbc1bd059e8bcf3b7cda85
zkung707224 <zkung@wisc.edu>
2025-01-27 11:20:35 -0600

    Update website

--- events/past_events_list.js
+++ events/past_events_list.js
@@ -10,6 +10,14 @@
 
 const past_events_list = [
   {
+    "date": { "month": "December", "day": "11", "time": "12:00 pm - 1:00 pm" },
+    "title": "WACM BiWeekly Lunch with Google Engineer",
+    "description": "Join us for our last biweekly lunch next week December 11th at 12-1pm in CS1240!! We will be having a Google engineer and UW-Madison alum as a guest speaker to share her story and experience as a woman in tech. Come get some Ian's pizza and learn!",
+    "imgSrc": "images/Biweekly121124.png",
+    "rsvpLink": "https://forms.gle/vSARB9pi9UwMATuT7",
+    "location": "Computer Sciences 1240"
+  },
+  {
     "date": { "month": "December", "day": "2", "time": "4:00 pm - 5:00 pm" },
     "title": "Internship Prep Talk",
     "description": "Join our President Kayley for the annual WACM Internship Preparation Talk, designed to help students navigate the internship application process with confidence. Kayley will share tips on crafting strong resumes, preparing applications, standing out in interviews, and exploring alternative ways to gain valuable experience if your internships aren’t secured. A mini resume review will also take place—upload your resume through the RSVP form for a chance to receive personalized feedback during the session!\nThe talk will conclude with a Q&A, giving attendees an opportunity to ask questions and receive personal advice. While this event is geared toward underclassmen and WACM members, all students are welcome to attend. Don’t miss this opportunity to gain invaluable insights and jumpstart your career in the computer science major!",
--- events/upcoming_events_list.js
+++ events/upcoming_events_list.js
@@ -10,11 +10,11 @@
 
 const upcoming_events_list = [
 {
-  "date": { "month": "December", "day": "11", "time": "12:00 pm - 1:00 pm" },
-  "title": "WACM BiWeekly Lunch with Google Engineer",
-  "description": "Join us for our last biweekly lunch next week December 11th at 12-1pm in CS1240!! We will be having a Google engineer and UW-Madison alum as a guest speaker to share her story and experience as a woman in tech. Come get some Ian's pizza and learn!",
-  "imgSrc": "images/Biweekly121124.png",
-  "rsvpLink": "https://forms.gle/vSARB9pi9UwMATuT7",
-  "location": "Computer Sciences 1240"
+  "date": { "month": "February", "day": "5", "time": "3:00pm - 5:00pm" },
+  "title": "WACM at the CS Org Fair",
+  "description": "WACM will be at the CS Student Engagement & Organization Fair! Come stop by and say hi.",
+  "imgSrc": "images/orgfair1272025.png",
+  "rsvpLink": "",
+  "location": "CS Lobby"
 }
 ];
